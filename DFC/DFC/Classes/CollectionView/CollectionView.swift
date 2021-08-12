e2dea09af41cf86939645d91630b2dc7463da60b
markojovanov <markojovanov999@gmail.com>
2021-08-12 20:47:46 +0200

    Update Prototype+CollectionView

--- DFC/DFC/Classes/CollectionView/CollectionView.swift
+++ DFC/DFC/Classes/CollectionView/CollectionView.swift
@@ -1,26 +1,33 @@
 import UIKit
 
 class CollectionView: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
+    let titleCell = ["Загуба на килограми","2","3","4"]
+    let subtitleCell = ["Фокус на кардио вежби и тренинг со голем интензитет","2","3","4"]
+    @IBOutlet var titleLabel: DFCLabel! {
+        didSet {
+            titleLabel.configure(title: "Која е твојата цел?")
+        } 
+    }
     @IBOutlet var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
         collectionView.delegate = self
     }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return 4
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
-        cell.backgroundColor = .red
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        cell.titleCellLabel.text = titleCell[indexPath.item]
+        cell.subtitleCellLabel.text = subtitleCell[indexPath.item]
+        cell.layer.cornerRadius = 18
+        cell.backgroundColor = .cyan
+        cell.layer.borderWidth = 1
+        cell.layer.borderColor = UIColor.black.cgColor
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let size = CGSize(width: 250, height: 120)
-        return size
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 20
+        return CGSize(width: 350, height: 100)
     }
 }
--- DFC/DFC/Classes/CollectionView/CollectionViewCell.swift
+++ DFC/DFC/Classes/CollectionView/CollectionViewCell.swift
@@ -1,5 +1,21 @@
 import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
-    
+    @IBOutlet var titleCellLabel: UILabel! {
+        didSet {
+            titleCellLabel.font.withSize(16)
+        }
+    }
+    @IBOutlet var subtitleCellLabel: UILabel! {
+        didSet {
+            subtitleCellLabel.preferredMaxLayoutWidth = 350
+            subtitleCellLabel.font.withSize(16)
+            subtitleCellLabel.baselineAdjustment = .alignCenters
+            subtitleCellLabel.contentMode = .topLeft
+            subtitleCellLabel.sizeToFit()
+            subtitleCellLabel.numberOfLines = 0
+            subtitleCellLabel.lineBreakMode = .byWordWrapping
+            subtitleCellLabel.textColor = .darkGray
+        }
+    }
 }
